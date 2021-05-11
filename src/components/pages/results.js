151b47f4a510cb2814fee9dff7989d03e959f89b
jesus1989garcia <jesus1989garcia@gmail.com>
2021-05-11 20:34:12 +0200

    results added

--- src/components/pages/results.js
+++ src/components/pages/results.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom';
 
 
 function Results(props){
@@ -25,6 +26,24 @@ function Results(props){
 
   return(
     <div className="results-container" >
+      { results.count ? results.matches.map(match =>
+        <div className="results-card"id={match.id}>
+          <div>
+            <Link className="link-team" to={`/team/${match.homeTeam.id}`}><p className={match.homeTeam.id == props.match.params.id ? "team-sel" : "team-vs"}>{match.homeTeam.name}</p></Link>
+          </div>
+          <div>
+            <p>{match.score.fullTime.homeTeam}</p>
+          </div>
+          { !match.score.winner ? <p>tbh</p> : null}
+          <div>
+          <p>{match.score.fullTime.awayTeam}</p>
+          </div>
+          <div>
+            <Link className="link-team" to={`/team/${match.homeTeam.id}`}><p className={match.awayTeam.id == props.match.params.id ? "team-sel" : "team-vs"}>{match.awayTeam.name}</p></Link>
+          </div>
+      
+        </div>
+        ) : ""}
       
 
     </div>
